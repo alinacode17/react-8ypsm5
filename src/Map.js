7fb5a44fbdf04504db0cdb7d4d3d1b17486fcd28
alinacode17 <alinawebdeveloper@gmail.com>
2023-02-24 12:55:19 +0000

    added light theme to the map

--- src/Map.js
+++ src/Map.js
@@ -2,25 +2,182 @@ import React, { useState } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import uuid from 'react-uuid';
 
-function NumberMarker(props) {
-  const { index } = props;
-
-  const markerStyle = {
-    backgroundColor: 'pink',
-    borderRadius: '50%',
-    padding: '5px',
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: '14px',
-    width: '40px',
-    height: '40px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  };
-
-  return <div style={markerStyle}>{index + 1}</div>;
-}
+const lightTheme = [
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#e9e9e9',
+      },
+      {
+        lightness: 17,
+      },
+    ],
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 17,
+      },
+    ],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 29,
+      },
+      {
+        weight: 0.2,
+      },
+    ],
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 18,
+      },
+    ],
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 16,
+      },
+    ],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f5f5f5',
+      },
+      {
+        lightness: 21,
+      },
+    ],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#dedede',
+      },
+      {
+        lightness: 21,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.stroke',
+    stylers: [
+      {
+        visibility: 'on',
+      },
+      {
+        color: '#ffffff',
+      },
+      {
+        lightness: 16,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.text.fill',
+    stylers: [
+      {
+        saturation: 36,
+      },
+      {
+        color: '#333333',
+      },
+      {
+        lightness: 40,
+      },
+    ],
+  },
+  {
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off',
+      },
+    ],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [
+      {
+        color: '#f2f2f2',
+      },
+      {
+        lightness: 19,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.fill',
+    stylers: [
+      {
+        color: '#fefefe',
+      },
+      {
+        lightness: 20,
+      },
+    ],
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [
+      {
+        color: '#fefefe',
+      },
+      {
+        lightness: 17,
+      },
+      {
+        weight: 1.2,
+      },
+    ],
+  },
+];
 
 function Map(props) {
   const mapContainerStyle = {
@@ -64,6 +221,7 @@ function Map(props) {
           mapContainerStyle={mapContainerStyle}
           center={center}
           zoom={12}
+          options={{ styles: lightTheme }}
         >
           {props.locations.map((location, index) => (
             <Marker
@@ -73,7 +231,6 @@ function Map(props) {
                 lat: Number(location.latitude),
                 lng: Number(location.longitude),
               }}
-              options={mapOptions}
             ></Marker>
           ))}
         </GoogleMap>
